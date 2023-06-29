59c3644c9dc498b98b6d5eff98fdcac2aaa323af
Arthur Morgado Teixeira <arthurmorgado7751@gmail.com>
2023-06-29 09:58:50 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-import doctest
+import traceback
 
 
 class DFA:
 
     def __init__(self, Q, Sigma, delta, q0, F):
-        self.Q = Q
-        self.Sigma = Sigma
-        self.delta = delta
-        self.q0 = q0
-        self.F = F
-
-    def __repr__(self):
-        return f"DFA ({self.Q},\n\t{self.Sigma},\n\t{self.delta},\n\t{self.q0},\n\t{self.F}"
+        self.Q = Q  # Conjunto de ESTADOS possíveis.
+        self.Sigma = Sigma  # Alfabeto do autômato. (Símbolos que seu autômato irá ler)
+        self.delta = delta  # Representação de transição de estados. Segue o exemplo do diagrama do automato.
+        self.q0 = q0  # Estado inicial
+        self.F = F  # Estados finais
 
     def run(self, w):
         q = self.q0
-        while w != "":
-            q = self.delta[(q, w[0])]
-            w = w[1:]
-        return print(q in self.F)
+        try:
+            while w != "":
+                q = self.delta[(q, w[0])]
+                w = w[1:]
+            return print(f"{q in self.F}\n")
+        except KeyError:
+            print(f"\nERRO: Input fora do alfabeto!\n")
 
+    def runMore(self):
+        while True:
+            w = input("Input: ")
+            if w == "":
+                break
+            else:
+                DFA.run(self, w)
+
+    def __repr__(self):
+        return f"""---DFA (Deterministic finite automaton)---\nEstados possiveis: {self.Q}\nAlfabeto: {self.Sigma}
+Transição dos estados: {self.delta}\nEstado inicial: {self.q0}\nEstados finais: {self.F}"""
+
+
+""" O char passado entre aspas duplas, é o alfabeto do seu Autômato. Você pode alterar ele para outros valores
+caso queira outro formato de Input."""
+
+"""Os números passado como 0, 1 e 2 representam os estados do autômato. Eles seguem o modelo do autômato."""
 
 D0 = DFA({0, 1, 2}, {"a", "b"},
          {(0, "a"): 0, (0, "b"): 1,
@@ -28,4 +45,4 @@ D0 = DFA({0, 1, 2}, {"a", "b"},
          0,
          {0, 1})
 
-D0.run("abbbb")
+D0.runMore()
